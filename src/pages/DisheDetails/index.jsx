2e920ac3cecbe2a2be53430c888fb500c8b1e9a3
Nicolas Sanatana Kruger <nicolassantanakruger@gmail.com>
2023-10-09 21:10:26 -0300

    finish: dish details apiREST

--- src/pages/DisheDetails/index.jsx
+++ src/pages/DisheDetails/index.jsx
@@ -1,4 +1,5 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 import { PiReceipt } from "react-icons/pi"
 import { AiOutlinePlus } from "react-icons/ai"
@@ -6,21 +7,57 @@ import { AiOutlineMinus } from "react-icons/ai"
 
 import { Container } from "./styles";
 
-import { Header } from "../../components/Header";
-import { Footer } from "../../components/Footer";
-import { BackButton } from "../../components/BackButton";
-import { Button } from "../../components/Button";
+import { Header } from "../../components/Header"
+import { Footer } from "../../components/Footer"
+import { BackButton } from "../../components/BackButton"
+import { Button } from "../../components/Button"
 
 import Ravanelo from "../../assets/Ravanelo.png"
 
+import { useAuth } from "../../hooks/auth"
+
+import { api } from "../../services/api"
+
 export const DisheDetails = () => {
-    let $isadmin = "true"
+    const { user } = useAuth()
+
+    let $isadmin = user.roles === "admin" ? "true" : "false"
 
     const navigate = useNavigate()
+    const params = useParams()
+
+    const [imgFile, setImgFile] = useState(null)
+    const [name, setName] = useState("")
+    const [ingredients, setIngredients] = useState([])
+    const [category, setCategory] = useState("meal")
+    const [price, setPrice] = useState("")
+    const [description, setDescription] = useState("")
+
+    const img = `${api.defaults.baseURL}/files/${imgFile}`
 
     const handleToEdit = () => {
-        navigate("/edit")
+        navigate(`/edit/${params.id}`)
     }
+
+    useEffect(() => {
+        const fetchDish = async() => {
+            try{
+                const response = await api.get(`/dishes/${params.id}`)
+                const dish = response.data
+
+                setImgFile(dish.img)
+                setName(dish.name)
+                setIngredients(dish.ingredients.map(ingredient => ingredient.name))
+                setCategory(dish.category)
+                setPrice(dish.price)
+                setDescription(dish.description)
+            } catch(error){
+                error.response ? alert(error.response.data.message) : alert("Não foi possivel carregar o prato. Tente Novamente!")
+            }
+        }
+
+        fetchDish()
+    }, [])
 
     return(
         <Container>
@@ -30,20 +67,21 @@ export const DisheDetails = () => {
                 <BackButton className="back-button" />
 
                 <div className="dishe">
-                    <img src={Ravanelo} alt="Imagem do prato" />
+                    <img src={img} alt="Imagem do prato" />
 
                     <div className="dishe-informations">
-                        <h1>Salada Ravaleno</h1>
+                        <h1>{name}</h1>
 
-                        <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim.</p>
+                        <p>{description}</p>
 
                         <ul>
-                            <li>alface</li>
-                            <li>cebola</li>
-                            <li>pão naan</li>
-                            <li>pepino</li>
-                            <li>rabanete</li>
-                            <li>tomate</li>
+                            {
+                                ingredients.map((ingredient, index) => {
+                                    return(
+                                        <li key={String(index)} >{ingredient}</li>
+                                    )
+                                })
+                            }
                         </ul>
 
                         <Button title="Editar prato" className={$isadmin === "true" || "hidden"} onClick={handleToEdit} />
